6791a87c91e6c409f6eb5aefee9541ad5e67abd1
jonatas <jonatas.olialmeida@gmail.com>
2022-01-20 10:29:57 -0300

    atualização

--- src/index.js
+++ src/index.js
@@ -77,4 +77,32 @@ processed = processArg(7);
 
 console.log(processed);
 
+// exercicio
+
+/**
+ * Escreva a função nextInLine, que recebe um array (arr) e um número (item) como argumentos.
+
+Adicione o número no final do array e então remova o primeiro elemento do array.
+
+A função nextInLine deve, em seguida, retornar o elemento que foi removido.
+ */
+
+function nextInLine(arr, item) {
+  // Altere apenas o código abaixo desta linha
+  arr.push(item)
+  let itemRemove = arr.shift();
+  return itemRemove;
+  // Altere apenas o código acima desta linha
+}
+
+// Configuração
+const testArr = [1, 2, 3, 4, 5];
+
+// Exibir o código
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+
+
 app.listen(3333);
