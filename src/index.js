d6d25f81bf49e8a04579dcf32339ea829cc87463
jonatas <jonatas.olialmeida@gmail.com>
2022-01-28 20:35:07 -0300

    up

--- src/index.js
+++ src/index.js
@@ -306,6 +306,54 @@ const person = {
     }
   };
   
+//   Usar a sintaxe de classe para criar uma função construtora
+
+/**
+ * ES6 fornece uma nova sintaxe para criar objetos, usando a palavra-chave class.
+
+Deve ser notado que a sintaxe class é apenas sintaxe, um syntatical sugar. JavaScript ainda não oferece suporte completo ao paradigma orientado a objetos, ao contrário do que acontece em linguagens como Java, Python, Ruby, etc.
+
+No ES5, geralmente definimos uma função construtora (constructor function) e usamos a palavra-chave new para instanciar um objeto.
+ */
+
+// var SpaceShuttle = function(targetPlanet){
+//     this.targetPlanet = targetPlanet;
+//   }
+//   var zeus = new SpaceShuttle('Jupiter');
+  
+// A sintaxe class simplesmente substitui a criação da função construtora (constructor):
+
+class SpaceShuttle {
+    constructor(targetPlanet) {
+      this.targetPlanet = targetPlanet;
+    }
+  }
+  const zeus = new SpaceShuttle('Jupiter');
+
+  /**
+   * 
+
+Use a palavra-chave class e declare o método constructor para criar a classe Vegetable.
+
+A classe Vegetable permite criar um objeto vegetal com a propriedade name que é passada ao constructor.
+
+   */
+
+// Altere apenas o código abaixo desta linha
+class Vegetable {
+    constructor(name){
+      this.name = name;
+    }
+  }
+  // Altere apenas o código acima desta linha
+  console.log(`
+  *******************
+  *******************
+  `)
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // Deve exibir 'carrot'
+
+
   
 console.log("Rodando")
 app.listen(3030);
